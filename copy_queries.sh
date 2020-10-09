26f6de212b38f41366c493aec8c82d7b6fd26854
jayfuhrman <48569483+jayfuhrman@users.noreply.github.com>
2020-10-09 16:58:47 -0400

    Update copy_queries.sh

--- copy_queries.sh
+++ copy_queries.sh
@@ -1,5 +1,5 @@
-GCAMDIR=/home/jgf5fz/gcam_5_2_1_beta
-INSTALLDIR=/home/jgf5fz/gcam_install
+#GCAMDIR=/home/jgf5fz/gcam_5_2_1_beta
+#INSTALLDIR=/home/jgf5fz/gcam_install
 QUERYDIR=${INSTALLDIR}/queries
 
 cp ${QUERYDIR}/exe/xmldb_batch_* ${GCAMDIR}/exe 
