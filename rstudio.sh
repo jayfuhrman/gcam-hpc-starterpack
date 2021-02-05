ef1d1611eef4783357a4e44b7e83511ba88c8ae0
jayfuhrman <48569483+jayfuhrman@users.noreply.github.com>
2021-02-05 16:05:03 -0500

    add startup scripts for python and Rstudio

---
+++ rstudio.sh
@@ -0,0 +1,5 @@
+#For updates on changes to launching Rstudio visit:
+#https://www.rc.virginia.edu/userinfo/rivanna/software/r/
+module load goolf/7.1.0_3.1.4 R
+module load rstudio
+rstudio &
---
+++ spyder.sh
@@ -0,0 +1,2 @@
+module load anaconda/5.2.0-py3.6
+spyder
